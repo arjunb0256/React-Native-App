9dfcf0a656b6d33bfd321235eb8a80d341703134
abagri0256 <abagri0256@langleyschools.ca>
2024-02-21 17:39:49 -0800

    added form and submit button, back button removed

--- assets/Screens/RegisterScreen.js
+++ assets/Screens/RegisterScreen.js
@@ -1,4 +1,4 @@
-import * as React from "react";
+import React, { useState } from "react";
 import {
   View,
   Text,
@@ -6,23 +6,50 @@ import {
   StyleSheet,
   TouchableOpacity,
   ImageBackground,
+  TextInput,
+  Alert,
 } from "react-native";
 
 function RegisterScreen({ navigation }) {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleSubmit = () => {
+    Alert.alert("Credentials", `${username} + ${password}`);
+  };
+
   return (
     <View style={styles.container}>
       <ImageBackground
         source={require("../images/HomeBackground.png")}
         style={{ flex: 1, width: "100%" }}
       >
-        <Text style={styles.Text}>Register</Text>
+        <Text style={styles.Text}>Login</Text>
 
-        <TouchableOpacity
-          style={styles.Back}
-          onPress={() => navigation.navigate("Home")}
-        >
-          <Text style={styles.BackText}>Back</Text>
-        </TouchableOpacity>
+        <View style={styles.formcontainer}>
+          <Text style={styles.FormText}>Username:</Text>
+          <TextInput
+            value={username}
+            onChangeText={(text) => setUsername(text)}
+            placeholder="Username"
+            placeholderTextColor="white"
+            style={styles.input}
+          />
+
+          <Text style={styles.FormText}>Password:</Text>
+          <TextInput
+            value={password}
+            onChangeText={(text) => setPassword(text)}
+            placeholder="Password"
+            placeholderTextColor="white"
+            secureTextEntry
+            style={styles.input}
+          />
+
+          <TouchableOpacity style={styles.Submit} onPress={handleSubmit}>
+            <Text style={styles.SubmitText}>Submit</Text>
+          </TouchableOpacity>
+        </View>
       </ImageBackground>
     </View>
   );
@@ -34,25 +61,64 @@ const styles = StyleSheet.create({
     alignItems: "center",
     justifyContent: "flex-start",
   },
+  formcontainer: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center",
+    height: 300,
+    width: 300,
+    color: "#fff",
+  },
   Text: {
     color: "#fff",
     fontSize: 50,
     marginTop: 150,
-    marginLeft: 105,
+    marginLeft: 140,
+    marginBottom: 100,
+  },
+  FormText: {
+    fontSize: 16,
+    color: "#fff",
+    marginLeft: 90,
+  },
+  Submit: {
+    alignItems: "center",
+    backgroundColor: "#DDDDDD",
+    padding: 10,
+    width: 250,
+    height: 50,
+    justifyContent: "center",
+    borderRadius: 10,
+    marginTop: 10,
+    marginLeft: 85,
+    marginBottom: 180,
+  },
+  SubmitText: {
+    fontSize: 20,
   },
   Back: {
     alignItems: "center",
     backgroundColor: "#DDDDDD",
     padding: 10,
-    width: 300,
-    height: 60,
+    width: 200,
+    height: 40,
     justifyContent: "center",
-    marginTop: 500,
+    marginTop: 200,
     borderRadius: 10,
     marginLeft: 40,
   },
   BackText: {
     fontSize: 20,
+  },
+  input: {
+    height: 40,
+    width: 200,
+    borderColor: "#000",
+    borderWidth: 0.5,
+    borderRadius: 5,
+    padding: 10,
+    marginBottom: 10,
+    marginLeft: 90,
   },
 });
 
